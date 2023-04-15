fd431505791e496d20f98f4c6f32f266fb8436a2
Yousaf Nabi <you@saf.dev>
2023-04-15 16:46:11 +0100

    ci: update pact-ruby-standalone across pact-node + pact-js-core

--- script/create-pr-to-update-pact-ruby-standalone.sh
+++ script/create-pr-to-update-pact-ruby-standalone.sh
@@ -25,3 +25,22 @@ git push --set-upstream origin ${BRANCH_NAME}
 hub pull-request --message "${TYPE}: update standalone to ${STANDALONE_VERSION}"
 
 git checkout master
+
+## Also raise a PR against pact-node-arm64
+
+git checkout pact-node-arm64
+git checkout standalone/install.ts
+git pull origin pact-node-arm64
+
+git checkout -b ${BRANCH_NAME}-pact-node-arm64
+
+cat standalone/install.ts | sed "s/export const PACT_STANDALONE_VERSION.*/export const PACT_STANDALONE_VERSION = '${STANDALONE_VERSION}';/" > tmp-install
+mv tmp-install standalone/install.ts
+
+git add standalone/install.ts
+git commit -m "${TYPE}: update standalone to ${STANDALONE_VERSION}"
+git push --set-upstream origin ${BRANCH_NAME}-pact-node-arm64
+
+hub pull-request --message "${TYPE}: update standalone to ${STANDALONE_VERSION}"
+
+git checkout master
