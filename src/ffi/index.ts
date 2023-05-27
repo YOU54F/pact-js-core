2cd224dbbb49b06ab98f4e1e05d38aa45b497d16
Yousaf Nabi <you@saf.dev>
2023-05-27 05:20:18 +0100

    feat: Prebuild pact_ffi pact.node + publish in npm pkg - (win-32|linux|darwin)-x64 (linux|darwin)-arm64

--- src/ffi/index.ts
+++ src/ffi/index.ts
@@ -1,12 +1,61 @@
-import path from 'path';
+import path from 'node:path';
 import logger, { DEFAULT_LOG_LEVEL } from '../logger';
 import { LogLevel } from '../logger/types';
 import { Ffi } from './types';
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const ffiLib: Ffi = require('node-gyp-build')(path.join(__dirname, '..', '..'));
-
 export const PACT_FFI_VERSION = '0.4.0';
+
+// supported prebuilds
+// darwin-arm64
+// darwin-x64
+// linux-arm64
+// linux-x64
+// win32-x64
+
+const supportedPlatforms = [
+  'darwin-arm64',
+  'darwin-x64',
+  'linux-arm64',
+  'linux-x64',
+  'win32-x64',
+];
+const platform = `${process.platform}-${process.arch}`;
+const supportedPlatformsMessage = `Supported platforms are: \n\n - ${supportedPlatforms.join(
+  '\n - '
+)}\n`;
+const detectedMessage = `We detected your platform as: \n\n - ${platform}\n`;
+
+if (!supportedPlatforms.includes(platform)) {
+  console.log(supportedPlatformsMessage);
+  console.log(detectedMessage);
+  console.error(`Unsupported platform: ${platform}`);
+  process.exit(1);
+}
+
+const loadPath =
+  process.env['PACT_NAPI_NODE_LOCATION'] ||
+  path.join(__dirname, '..', '..', 'prebuilds', platform, 'node.napi.node');
+const loadPathMessage = `: loading native module from: \n\n - ${path.resolve(
+  loadPath
+)} ${
+  process.env['PACT_NAPI_NODE_LOCATION']
+    ? '\n - source: PACT_NAPI_NODE_LOCATION\n'
+    : '\n   source: prebuilds \n\n - You can override via PACT_NAPI_NODE_LOCATION\n'
+}`;
+let ffiLib: Ffi;
+const importedLib = import(loadPath);
+importedLib
+  .then((lib) => {
+    ffiLib = lib.default;
+    console.log(`Success ${loadPathMessage}`);
+  })
+  .catch((error) => {
+    console.log(supportedPlatformsMessage);
+    console.log(detectedMessage);
+    console.log(`Failed ${loadPathMessage}`);
+    console.error(`Failed to load native module: ${error}`);
+    process.exit(1);
+  });
 
 let ffi: typeof ffiLib;
 let ffiLogLevel: LogLevel;
