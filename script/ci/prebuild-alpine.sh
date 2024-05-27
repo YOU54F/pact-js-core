79abe28b4e5c79cec21ae0c97b739c33c4589871
Yousaf Nabi <you@saf.dev>
2024-05-27 15:34:51 +0100

    fix(build): run ranlib on stripped libpact_ffi.a on musl targets

--- script/ci/prebuild-alpine.sh
+++ script/ci/prebuild-alpine.sh
@@ -14,7 +14,8 @@ apk add bash curl python3 make g++
 rm -rf build node_modules
 npm ci --ignore-scripts
 export npm_config_target=${NODE_VERSION}
-npx --yes prebuildify@${PREBUILDIFY_VERSION} --napi --libc musl --tag-libc --name ${PREBUILD_NAME}
+ranlib ffi/*/libpact_ffi.a
+npx --yes prebuildify@${PREBUILDIFY_VERSION} --napi --libc musl --strip --tag-libc --name ${PREBUILD_NAME}
 ls prebuilds/**/*
 ARCH=$(uname -m | tr '[:upper:]' '[:lower:]')
 case $ARCH in
