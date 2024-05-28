2195a1acde72c171dbfc05bc999680df365688ac
Yousaf Nabi <you@saf.dev>
2024-05-28 01:27:05 +0100

    chore: fork release prep

--- script/ci/lib/create_npmrc_file.sh
+++ script/ci/lib/create_npmrc_file.sh
@@ -9,10 +9,10 @@ require_env_var NODE_AUTH_TOKEN
 set +x #Don't echo the NPM key
 
 NPMRC_FILE=.npmrc
-echo  "@pact-foundation:registry=https://registry.npmjs.org/" > $NPMRC_FILE
+echo  "@@you54f:registry=https://registry.npmjs.org/" > $NPMRC_FILE
 echo "//registry.npmjs.org/:_authToken=${NODE_AUTH_TOKEN}" >> $NPMRC_FILE
-echo "//registry.npmjs.org/:username=pact-foundation" >> $NPMRC_FILE
-echo "//registry.npmjs.org/:email=pact-foundation@googlegroups.com" >> $NPMRC_FILE
+echo "//registry.npmjs.org/:username=@you54f" >> $NPMRC_FILE
+echo "//registry.npmjs.org/:email=you54f@saf.dev" >> $NPMRC_FILE
 echo "//registry.npmjs.org/:always-auth=true" >> $NPMRC_FILE
 
 set -x
