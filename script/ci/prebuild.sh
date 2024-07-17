6eb92980eb2e9bd8d24dcd16e4693565d2212916
Yousaf Nabi <you@saf.dev>
2024-07-17 20:16:25 +0100

    chore: uncomment lib dl

--- script/ci/prebuild.sh
+++ script/ci/prebuild.sh
@@ -34,7 +34,7 @@ npm --version
 echo "OS: $OS"
 echo "ARCH: $ARCH"
 
-# ./script/download-libs.sh
+./script/download-libs.sh
 npm ci --ignore-scripts
 export npm_config_target=${NODE_VERSION}
 npx --yes prebuildify@${PREBUILDIFY_VERSION} --napi --name ${PREBUILD_NAME}
@@ -90,4 +90,4 @@ case $OS in
     ;;
 esac
 ls
-# rm -rf ffi build
+rm -rf ffi build
