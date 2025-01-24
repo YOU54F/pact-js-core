24a96ac7ad1e5417b9cece94fc02a6a7462c55c0
Yousaf Nabi <yousaf@pactflow.io>
2025-01-24 15:26:56 +0000

    chore(ci): use prebuilds to speed up ci runs

--- script/ci/unpack-and-test.sh
+++ script/ci/unpack-and-test.sh
@@ -5,11 +5,13 @@ set -u
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd)" # Figure out where the script is running
 . "$SCRIPT_DIR"/../lib/robust-bash.sh
 
-ls -1
-ls -1 artifact*
-mkdir -p prebuilds
-mv artifact*/*.tar.gz .
-ls *.gz |xargs -n1 tar -xzf
+if [ ! -d "prebuilds" ]; then
+    ls -1
+    ls -1 artifact*
+    mkdir -p prebuilds
+    mv artifact*/*.tar.gz .
+    ls *.gz | xargs -n1 tar -xzf
+fi
 
 "$SCRIPT_DIR"/../download-plugins.sh
 # Use the determined platform
